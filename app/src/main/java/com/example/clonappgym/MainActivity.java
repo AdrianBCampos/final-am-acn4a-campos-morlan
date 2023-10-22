308e8926cf3cab9eef7de7dce1b3c9e5cd835cc2
AdrianBCampos <adrian.campos@davinci.edu.ar>
2023-10-22 18:19:09 -0300

    feat: testing

---
+++ app/src/main/java/com/example/clonappgym/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.clonappgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //LinearLayout contentNews = findViewById(R.id.contenedorBotones);
+        //TextView nuevaPublicacion = new TextView(this);
+        //nuevaPublicacion.setText("Novedades de Spinning");
+        //TextView otraPublicacion = new TextView(this);
+        //otraPublicacion.setText("Horarios de Spinning");
+
+        //contentNews.addView(nuevaPublicacion);
+        //contentNews.addView(otraPublicacion);
+
+        LinearLayout contentAfiche = findViewById(R.id.contenedorBotones);
+        ImageView nuevoAfiche = new ImageView(this);
+        int afiche = 0;
+        afiche =(int) (Math.random()*2)+1;
+        if (afiche ==1)
+        {
+            nuevoAfiche.setImageResource(R.drawable.afiche1);
+            contentAfiche.addView(nuevoAfiche);
+        }
+        else
+        {
+            nuevoAfiche.setImageResource(R.drawable.afiche3);
+            contentAfiche.addView(nuevoAfiche);
+        }
+
+
+
+    }
+}
