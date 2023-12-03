a48dfc3d9c95b1cdfb3b3425f09bb423437406dc
AdrianBCampos <adrian.campos@davinci.edu.ar>
2023-12-03 14:29:14 -0300

    feat: onClick API

--- app/src/main/java/com/example/clonappgym/ApiActivity.java
+++ app/src/main/java/com/example/clonappgym/ApiActivity.java
@@ -2,39 +2,46 @@ package com.example.clonappgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ApiActivity extends AppCompatActivity {
-    public void checkConnectionOnClick (View v){
-        checkConnection();
+    public void checkConnectionOnClick2(View v){
+
+        checkConnection2();
+
     }
 
 
-    public void checkConnection(){
+    public void checkConnection2(){
         ConnectivityManager connectivityManager=(ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
-        TextView mensaje2 = findViewById(R.id.msj_no_internet2);
-        Button button2 = findViewById(R.id.button33);
+
+        TextView msj = findViewById(R.id.textView);
+        Button button4 = findViewById(R.id.button4);
+        Button volver = findViewById(R.id.volver_mp);
         if (networkInfo != null && networkInfo.isConnected()){
             Log.i ("network.testing", "probando la red");
 
-            mensaje2.setVisibility (View.INVISIBLE);
-            button2.setVisibility(View.INVISIBLE);
+            msj.setText("Conectado a internet");
+            msj.setVisibility((View.VISIBLE));
+            volver.setVisibility(View.VISIBLE);
+            button4.setVisibility(View.INVISIBLE);
             GetApiGym ejercicios = new GetApiGym();
             ejercicios.execute("https://rickandmortyapi.com/api/character/?name=rick&status=alive");
 
         } else {
-            mensaje2.setText("Internet no disponible");
-            mensaje2.setVisibility(View.VISIBLE);
-            button2.setVisibility (View.VISIBLE);
+            msj.setText("No hay conexion a internet");
+            msj.setVisibility(View.VISIBLE);
+            button4.setVisibility(View.VISIBLE);
+            volver.setVisibility(View.VISIBLE);
         }
     }
 
@@ -42,11 +49,15 @@ public class ApiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_api);
-
-        checkConnection();
+        checkConnection2();
 
     }
 
+    public void paginaPrincipal(View v){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        startActivity(intent);
+    }
 
 
 
---
+++ app/src/main/java/com/example/clonappgym/GapInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.clonappgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GapInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gap_info);
+    }
+}
