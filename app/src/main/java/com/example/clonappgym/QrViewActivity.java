6cd254c5baa6d03cbab18f88d0c1a30507493615
AdrianBCampos <adrian.campos@davinci.edu.ar>
2023-12-03 17:19:48 -0300

    feat: add internet connection verification in "Obtener mi codigo QR" button

--- app/src/main/java/com/example/clonappgym/QrViewActivity.java
+++ app/src/main/java/com/example/clonappgym/QrViewActivity.java
@@ -64,14 +64,52 @@ public class QrViewActivity extends AppCompatActivity {
     }
 
     public void loadImage(View v) {
-        ImageView imageView = findViewById(R.id.imageQR);
-        String urlqr = "https://raw.githubusercontent.com/AdrianBCampos/imagenes/main/Codigo-qr.png";
-        Log.i("Testing", urlqr);
-        Downloader descarga = new Downloader(imageView);
-        descarga.execute(urlqr);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        Button volver = findViewById(R.id.obtenerQR_btn2);
+        ImageView fotoQr = findViewById(R.id.imageQR);
+
         TextView msj = findViewById(R.id.text_qr_activity);
-        msj.setText(R.string.este_es_tu_c_digo_qr_para_ingresar_al_gym);
-        msj.setBackgroundColor(+R.color.black);
+        Button obtenerQr = findViewById(R.id.obtenerQR_btn);
+        Button button5 = findViewById(R.id.button5);
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Log.i("network.testing", "probando la red");
+
+            button5.setVisibility(View.INVISIBLE);
+
+            fotoQr.setVisibility(View.VISIBLE);
+            msj.setText("(Hacé click en el botón para obtener tu codigo QR)");
+            msj.setVisibility((View.VISIBLE));
+            obtenerQr.setVisibility(View.VISIBLE);
+            volver.setVisibility(View.VISIBLE);
+            ImageView imageView = findViewById(R.id.imageQR);
+            String urlqr = "https://raw.githubusercontent.com/AdrianBCampos/imagenes/main/Codigo-qr.png";
+            Log.i("Testing", urlqr);
+            Downloader descarga = new Downloader(imageView);
+            descarga.execute(urlqr);
+
+            msj.setText(R.string.este_es_tu_c_digo_qr_para_ingresar_al_gym);
+            msj.setBackgroundColor(+R.color.black);
+
+
+        } else {
+            msj.setText("No hay conexion a internet");
+            msj.setVisibility(View.VISIBLE);
+            button5.setVisibility(View.VISIBLE);
+            volver.setVisibility(View.VISIBLE);
+            obtenerQr.setVisibility(View.INVISIBLE);
+            fotoQr.setVisibility(View.INVISIBLE);
+        }
+
+
+
+
+
+
+
 
 
     }
